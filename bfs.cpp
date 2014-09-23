0f5e5d504e0dba856ad3f761f3185c6498a0023a
mac_vbox <maoze365@gmail.cm>
2014-09-22 23:59:59 -0700

    dfs segment

--- bfs.cpp
+++ bfs.cpp
@@ -14,22 +14,24 @@ BFS::BFS(Task &task_info)
 	while(!Q.empty()){
 		parent_index = Q.front();
 		Q.pop();
+		task_info.nodes[parent_index].visited=1;
+		expansion_q.push(temp_children[i]);
+		if(parent_index == task_info.dst_index){
+			finished = 1;
+			return;
+		}
 
 		sort_children(parent_index, temp_children);
+		if (temp_children.size()==0)
+			return;
 		// real bfs loop
 		for(i = 0; i<temp_children.size(); i++)
 		{
 		    cout << "PUSH alphtical: " << task_info.nodes[temp_children[i]].name << endl; 
 			Q.push(temp_children[i]);
-			task_info.nodes[temp_children[i]].visited=1;
 			task_info.nodes[temp_children[i]].parent=parent_index;
 			task_info.nodes[temp_children[i]].path_cost=task_info.nodes[parent_index].path_cost + task_info.distance[parent_index][temp_children[i]] ;
-			expansion_q.push(temp_children[i]);
         	cout << "Cost so far: " << task_info.nodes[temp_children[i]].path_cost << endl;
-			if(temp_children[i] == task_info.dst_index){
-				finished = 1;
-				return;
-			}
 		}
 		temp_children.clear();
 	}
--- dfs.cpp
+++ dfs.cpp
@@ -4,8 +4,6 @@
 DFS::DFS(Task &task_info)
 {
     stack<int> S;
-	expansion_q.push(task_info.src_index);
-	task_info.nodes[task_info.src_index].visited = 1;
 	vector<int> temp_children;
 	int parent_index;
 	int i;
@@ -14,21 +12,25 @@ DFS::DFS(Task &task_info)
 	while(!S.empty()){
 		parent_index = S.top();
 		S.pop();
-
-		sort_children(parent_index, temp_children);
-		// real dfs loop
-        i=0;
-        cout << "PUSH alphtical: " << task_info.nodes[temp_children[i]].name << endl; 
-        S.push(temp_children[i]);
-        task_info.nodes[temp_children[i]].visited=1;
-        task_info.nodes[temp_children[i]].parent=parent_index;
-        task_info.nodes[temp_children[i]].path_cost=task_info.nodes[parent_index].path_cost + task_info.distance[parent_index][temp_children[i]] ;
-        expansion_q.push(temp_children[i]);
-        cout << "Cost so far: " << task_info.nodes[temp_children[i]].path_cost << endl;
-        if(temp_children[i] == task_info.dst_index){
+		task_info.nodes[parent_index].visited=1;
+        expansion_q.push(parent_index);
+        if(parent_index == task_info.dst_index){
         	finished = 1;
         	return;
         }
-		temp_children.clear();
-	}
+
+		sort_children(parent_index, temp_children);
+		if (temp_children.size()==0)
+			return;
+
+		// real dfs loop
+        for(i = temp_children.size()-1; i>=0; i--) {
+            cout << "PUSH alphtical: " << task_info.nodes[temp_children[i]].name << endl; 
+        	S.push(temp_children[i]);
+        	task_info.nodes[temp_children[i]].parent=parent_index;
+        	task_info.nodes[temp_children[i]].path_cost=task_info.nodes[parent_index].path_cost + task_info.distance[parent_index][temp_children[i]] ;
+        	cout << "Cost so far: " << task_info.nodes[temp_children[i]].path_cost << endl;
+        }
+        temp_children.clear();
+        }
 }
